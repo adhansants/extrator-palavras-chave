6da13a8325ccc4530bc9e99335b0fa254e219f1a
Adhan Martins dos Santos <143520724+adhansants@users.noreply.github.com>
2025-08-13 11:23:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
 function mostraPalavrasChave() {
-    alert('fui clicado!');
+const texto = document.querySelector('#entrada-de-texto')
+const campoResultado = documento.querySelector('#resultado-palavrachave');
+
+campoResultado.textContent = texto;
 }
+
